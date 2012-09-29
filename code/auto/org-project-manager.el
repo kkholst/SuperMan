389ea45e4d0ee02c027748eedffc7401b990c2e0
Thomas Gerds <grb615@linuxifsv007.(none)>
2012-09-29 08:29:24 +0200

    added make-directory of index function, see org-pro-find-index

--- code/auto/org-project-manager.el
+++ code/auto/org-project-manager.el
@@ -1,14 +1,5 @@
 (TeX-add-style-hook "org-project-manager"
  (lambda ()
-    (LaTeX-add-labels
-     "sec-1"
-     "sec-1-1"
-     "sec-1-1-1"
-     "sec-1-1-1-1"
-     "sec-1-1-1-2"
-     "sec-1-2"
-     "sec-1-2-1"
-     "sec-1-2-2")
     (TeX-add-symbols
      '("alert" 1)
      '("sfootnote" 1)
