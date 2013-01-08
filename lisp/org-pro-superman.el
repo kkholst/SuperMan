096d37355f08bd478f29bc5a5e7d602685b3ed81
tagteam <tag@biostat.ku.dk>
2013-01-08 16:22:45 +0100

    initial commit

---
+++ lisp/org-pro-superman.el
@@ -0,0 +1,42 @@
+;;; org-pro-superman.el --- org project manager
+
+;; Copyright (C) 2013  Thomas Alexander Gerds
+
+;; Authors:
+;; Thomas Alexander Gerds <tag@biostat.ku.dk>
+;; Klaus Kähler Holst <kkho@biostat.ku.dk>
+;;
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; An super project to manage all the other projects
+;; Q: Does the super project contain itself?
+;; A: Nice question. To find some answers please read logicomix. Get it here: www.logicomix.com
+
+;;; Code:
+
+(defun org-pro-superman ()
+  "Returns a super project for project management"
+  `("SuperManager"
+    (("location" . ,org-pro-default-directory)
+     ("index" . ,org-pro-file)
+     ("category" . "Super")
+     ("state" . "ACTIVE")
+     ("config" . "INDEX | AGENDA / TODO"))))
+
+(provide 'org-pro-superman)
+;;; org-pro-superman.el ends here
